c82faf835043bdcd3bbb513440a3cd3e4bb76af6
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2025-10-22 20:54:39 -0300

    Actualizacion en APP
    Se actualiza app para poder usar el navbar por importar boostrap agregando Routes y tambien el footer (tabien se agrego a APP)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarPacrima from "./components/NavbarPacrima";
+import Footer from "./components/Footer";
 
+// Páginas
 import Inicio from "./pages/Inicio";
-import Nosotros from "./pages/Nosotros";
 import Servicios from "./pages/Servicios";
+import Nosotros from "./pages/Nosotros";
 import Contacto from "./pages/Contacto";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import DetallesServicio from "./pages/DetallesServicio";
-
-import NavbarPacrima from "./components/NavbarPacrima";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <NavbarPacrima />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/servicios" element={<Servicios />} />
-        <Route path="/servicio/:id" element={<DetallesServicio />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Footer />
+      <div className="d-flex flex-column min-vh-100">
+        <NavbarPacrima />
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/servicios" element={<Servicios />} />
+            <Route path="/nosotros" element={<Nosotros />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer className="site-footer">
-      <p>© 2025 Pacrima - Servicios Profesionales</p>
+    <footer className="bg-dark text-light py-4 mt-auto">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h5>Pacrima</h5>
+            <p>Soluciones digitales profesionales y personalizadas para tu negocio.</p>
+          </Col>
+          <Col md={6} className="text-md-end">
+            <p>&copy; {new Date().getFullYear()} Pacrima - Todos los derechos reservados</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
